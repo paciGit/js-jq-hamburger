dd45535341fcfc927f793b32aeac895416cb8575
Vincenzo <megnavincenzo97@gmail.com>
2020-07-02 15:24:13 +0200

    Menu hamburger finito, inserito metodo (toogle) con parametri speed & easing

---
+++ main.js
@@ -0,0 +1,13 @@
+// Apertura menu hambuger utilizzando toggle ed i vari parametri
+$('.fa-bars').click(
+  function(){
+    $('.hamburger-menu').toggle('fast', 'swing');
+  }
+)
+
+// Chiusura menu hambuger utilizzando toggle ed i vari parametri
+$('.fa-times').click(
+  function(){
+    $('.hamburger-menu').toggle('fast', 'swing');
+  }
+)
